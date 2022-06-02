19aaf2a34b21d05a819de4030fac7a8b603b4189
ombhonsle16 <ombhonsle16@gmail.com>
2022-06-02 08:45:27 +0530

    Practical 18

---
+++ app/src/main/java/com/example/practical18/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.practical18;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText etUsername, etPassword;
+    Button btnStatus;
+    TextView tvLoginStatus;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        btnStatus = (Button) findViewById(R.id.button);
+        tvLoginStatus = (TextView) findViewById(R.id.tvLoginStatus);
+        etPassword.addTextChangedListener(TextChange);
+        etUsername.addTextChangedListener(TextChange);
+        check();
+    }
+
+    private TextWatcher TextChange=new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            check();
+        }
+    };
+    public void check(){
+        if(etUsername.getText().toString().equals("tonystark") && etPassword.getText().toString().equals("loveyou3000")){
+            btnStatus.setEnabled(true);
+        }else{
+            btnStatus.setEnabled(false);
+        }
+    }
+}
